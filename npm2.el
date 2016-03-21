c6f55b8564703c1bea39c78129a76f7883b99533
Emil Gjørup <limemloh@gmail.com>
2016-03-21 13:39:18 +0100

    feature to initialize the package.json

--- npm2.el
+++ npm2.el
@@ -1,44 +1,48 @@
 ;;; npm2.el --- NPM Node Package Manager
-
+;;
 ;; Copyright (C) 2016 Emil Gjørup
-
+;;
 ;; Author: Emil Gjørup <limemloh@gmail.com>
 ;; Keywords: npm
-
+;;
 ;;; Version: 0.0.1
-
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;; code:
 
 (require 'json)
 
 
-;; Helpers
-
-
-(defun npm2-create-package-json-string (name version description)
-  "Creates the package.json string"
-  (json-encode-list
-   (list (cons "name" name)
-         (cons "version" version)
-         (cons "description" description)
-         '(main . "index.js")
-         '(scripts . ((test . "echo \"Error: no test specified\" && exit 1")))
-         (cons "author" (format "%s <%s>" user-full-name user-mail-address))
-         '(license . "ISC"))))
-
+(defun npm2--find-project-root-directory ()
+  "Find the project root directory."
+  (file-name-nondirectory
+   (directory-file-name default-directory)))
 
 
 (defun npm2-init-package (name version description)
-  "Creates the package.json"
+  "Initialise project and create a package.json.
+NAME is a the project name.
+VERSION is the project version.
+DESCRIPTION is the project description."
   (interactive
-   (let ((inputstring (lambda (text default) (read-string (concat text " (default " default "): ") nil nil default)))
-         ())
-     (list (funcall inputstring "Enter project name" "NPM")
-           (funcall inputstring "Enter project version" "0.0.1")
-           (funcall inputstring "Enter project description" ""))))
+   (let ((project-root-directory (npm2--find-project-root-directory)))
+     (list (read-string (concat "Enter project name (default " project-root-directory "): ") nil nil project-root-directory)
+           (read-string "Enter project version: " "0.0.1")
+           (read-string "Enter project description: "))))
   (let ((json-encoding-pretty-print t))
     (with-temp-file "package.json"
-      (insert (create-package-json-string name version description)))))
+      (insert (json-encode-list
+               (list (cons "name" name)
+                     (cons "version" version)
+                     (cons "description" description)
+                     '(main . "index.js")
+                     '(scripts . ((test . "echo \"Error: no test specified\" && exit 1")))
+                     (cons "author" (format "%s <%s>" user-full-name user-mail-address))
+                     '(license . "ISC")))))))
+
 
 
 (provide 'npm2)
--- readme.md
+++ readme.md
@@ -1,11 +1,31 @@
 # npm2.el
+Emacs package with tools for working with [npm - Node Package Manager](https://www.npmjs.com/).
 
 # Installation
+So far npm2 is only to be found here on GitHub.
+```elisp
+(add-to-list 'load-path "~/path/to/dir/with/npm2")
+(require 'npm2)
+```
+
+
+If you want to use use-package:
+```elisp
+(use-package npm2
+  :load-path "~/path/to/dir/with/npm2")
+```
+
 
 # Features
-* TODO Generate package.json
-* TODO Package.json Major-mode
-* TODO Install npm packages
-* TODO Install from require("*package-name*")
+* Generate package.json. `npm2-init-package`
+..* DONE: Create package.json
+..* DONE: Suggest current directory as default name
+..* TODO: Check if a package.json already exist
+* Package.json Major-mode.
+* TODO: wrapper for npm run *script-name*.
+* TODO: Install npm packages.
+* TODO: Install from require("*package-name*").
+* TODO: Install npm2.el from melpa.
 
-# Contribute
+# License
+[GNU General Public License v3](./LICENSE)
